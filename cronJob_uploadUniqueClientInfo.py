88e99f4a9d32e42daf7ca0aeb37113393784ea0d
Israel-melendez <israel.melendez@ibm.com>
2024-02-07 03:07:34 -0600

    changes

--- cronJob_uploadUniqueClientInfo.py
+++ cronJob_uploadUniqueClientInfo.py
@@ -87,17 +87,20 @@ def get_credit_data(src_path):
     }
 
     # Create a list of JSON data for each POST request
-    payloads= [ {"number": f"{str(client_id)}","date":""} 
-               for client_id in client_ids ]
+    payloads= [ {
+        "number": f"{str(client_id)}",
+        "date": ""
+                 } 
+                for client_id in client_ids 
+                ]
 
     responses = []
 
     for payload in payloads:
-        # try:
-        logging.debug("payload", payload)
-        response = send_post_request(url, payload, headers) 
-        # except:
-        #     response = f"Credit data: Error getting data for {payload}" 
+        try:
+            response = send_post_request(url, payload, headers) 
+        except:
+            response = f"Credit data: Error getting data for {payload}" 
         responses.append(response)
 
     return responses 
